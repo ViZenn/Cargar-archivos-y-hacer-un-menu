1e889215794c0178733129ca8d7c2ac0c672ef0e
Vicente <vaiturriaga@uc.cl>
2021-06-30 22:24:56 -0400

    Primer Commit: codigo abre una GUI, selecciona un archivo excel y lo transforma en DF

---
+++ #Importar Archivos.py
@@ -0,0 +1,8 @@
+#Importar Archivos Abriendo Explorador
+import os
+import pandas as pd
+from tkinter.filedialog import askopenfilename
+
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+df=pd.read_excel(filename)
+df.describe()
