4dbc321b11dfa13e634ee1735e7601dce6bbbdec
Vicente <vaiturriaga@uc.cl>
2021-07-01 11:09:38 -0400

    agregue un print para ver como funciona el branch

--- #Importar Archivos.py
+++ #Importar Archivos.py
@@ -6,3 +6,6 @@ from tkinter.filedialog import askopenfilename
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 df=pd.read_excel(filename)
 df.describe()
+
+print('nueva Rama para el commit')
+print('otro print')
